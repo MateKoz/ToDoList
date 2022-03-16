30eb36a881ce5c0b8a9ba6827ba818c95f8e3462
MateuszKozlowski <mateuszkozlowski@climatik.pl>
2022-03-16 18:16:45 +0100

    Todo List - completed

--- main.js
+++ main.js
@@ -4,6 +4,14 @@ const addBtn = document.querySelector(`.btn-add`)
 const ulList = document.querySelector(`.todolist ul`);
 let newTodo
 
+let todoToEdit
+let popup = document.querySelector(`.popup`)
+let popupInfo = document.querySelector(`.popup-info`)
+let popupInput = document.querySelector(`.popup-input`)
+let popupAddBtn = document.querySelector(`.accept`)
+let popupCloseBtn = document.querySelector(`.cancel`)
+
+//Funkcja dodaje nowego taska.
 const addNewTask = () => {
     if (todoInput.value !== ``) {
         newTodo = document.createElement(`li`);
@@ -17,6 +25,7 @@ const addNewTask = () => {
     }
 }
 
+//Funkcja tworzy nowego LI wraz z buttonami (complete, edit, delete).
 const createToolsLi = () => {
     const toolsPanel = document.createElement(`div`);
     toolsPanel.classList.add(`tools`);
@@ -37,21 +46,68 @@ const createToolsLi = () => {
     toolsPanel.append(completeButton, editButton, deleteButton);
 }
 
+
 const checkClick = e => {
-    if(e.target.matches(`.complete`)) {
+    if (e.target.matches(`.complete`)) {
         e.target.closest(`li`).classList.toggle(`completed`);
         e.target.classList.toggle(`completed`)
 
     } else if (e.target.matches(`.edit`)) {
-        console.log(`edit`)
+        editTodo(e);
 
     } else if (e.target.matches(`.delete`)) {
         console.log(`delete`)
+        deleteTask(e);
     }
 }
 
-addBtn.addEventListener(`click`, addNewTask)
-ulList.addEventListener(`click`, checkClick)
+//Funkcja która wyświetla popup do edycji taska.
+const editTodo = (e) => {
+    todoToEdit = e.target.closest(`li`)
+    popupInput.value = todoToEdit.firstChild.textContent
+    popup.style.display = `flex`;
+}
+
+//Funkcja zamykająca popup do edycji taska.
+const closePopup = () => {
+    popup.style.display = `none`;
+    popupInfo.textContent = ``
+
+}
+
+//Edycja wybranego taska.
+const changeTodoText = () => {
+    if (popupInput.value !== ``) {
+        todoToEdit.firstChild.textContent = popupInput.value;
+        popup.style.display = `none`;
+        popupInfo.textContent = ``;
+
+    } else {
+        popupInfo.textContent = `Musisz podac jakąś treść!`
+    }
+}
+
+//Usuwanie taska z listy.
+const deleteTask = (e) => {
+    e.target.closest(`li`).remove();
+    const allTodos = ulList.querySelectorAll(`li`);
+    if (allTodos.length === 0) {
+        errorInfo.textContent = `Brak zadań na liście`
+    }
+}
+
+//Dodawanie nowego taska przez nacisnięcie ENTER;
+const enterKeyCheck = e => {
+    if(e.key === `Enter`) {
+        addNewTask()
+    }
+}
+
+todoInput.addEventListener('keyup', enterKeyCheck)
+popupAddBtn.addEventListener(`click`, changeTodoText);
+popupCloseBtn.addEventListener(`click`, closePopup);
+addBtn.addEventListener(`click`, addNewTask);
+ulList.addEventListener(`click`, checkClick);
 
 
 
